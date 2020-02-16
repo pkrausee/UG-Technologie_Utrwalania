4b0fc6f95e125de016da4203855a246b673e4df4
Pawel <apawell@o2.pl>
2020-02-16 15:31:28 +0100

    refactor

--- lab3/01_jdbc_wstep/db/runHSQLDBClient.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -cp ./db/hsqldb.jar  org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
--- lab3/01_jdbc_wstep/db/runHSQLDBServer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -cp ./db/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
